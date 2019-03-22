ec003df4a87e8583951666fcc04a86c0460d33b5
VinTran <tranquillo.vinnie@gmail.com>
2019-03-22 13:45:30 -0400

    New portfolio

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+//   $(document).ready(function(){
+//     $('.parallax').parallax();
+//   });
+
+
+//   var instance = M.Parallax.getInstance(elem);
+
+//   /* jQuery Method Calls
+//     You can still use the old jQuery plugin method calls.
+//     But you won't be able to access instance properties.
+
+//     $('.parallax').parallax('methodName');
+//     $('.parallax').parallax('methodName', paramName);
+//   */
+
+//  instance.destroy();
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
